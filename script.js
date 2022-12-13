f1af5f0acedb86637f53212d843f8d5a713b200d
GitHub1043 <69536208+GitHub1043@users.noreply.github.com>
2022-12-13 08:49:07 -0600

    Revert "test sticky nav"
    This reverts commit b34b1bfb6fe3a686f1801b7969834574641d2141.

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function () { myFunction() };
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
     } else {
-        navbar.classList.remove("sticky");
+        x.className = "navbar";
     }
 }
