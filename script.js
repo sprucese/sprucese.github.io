b34b1bfb6fe3a686f1801b7969834574641d2141
GitHub1043 <69536208+GitHub1043@users.noreply.github.com>
2022-12-13 08:47:57 -0600

    test sticky nav

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () { myFunction() };
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "navbar") {
-        x.className += " responsive";
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
     } else {
-        x.className = "navbar";
+        navbar.classList.remove("sticky");
     }
 }
