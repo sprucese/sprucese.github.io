642d04bf984499e044f80132a991a838072a1d4b
GitHub1043 <69536208+GitHub1043@users.noreply.github.com>
2022-12-13 08:31:01 -0600

    sticky nav

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
         x.className += " responsive";
     } else {
-        x.className = "topnav";
+        x.className = "navbar";
+    }
+}
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () { stickyNav() };
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNav() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
     }
 }
